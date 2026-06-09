552e7ff8b98726d27568f417f785bfefa20843d8
Razvan Mihailescu <razvan.mihailescu1996@gmail.com>
2026-06-09 13:38:02 +0300

    Avoid vaul portal in shared drawer

--- frontend/components/ui/drawer.tsx
+++ frontend/components/ui/drawer.tsx
@@ -52,7 +52,7 @@ function DrawerContent({
   ...props
 }: ComponentProps<typeof DrawerPrimitive.Content>) {
   return (
-    <DrawerPortal data-slot="drawer-portal">
+    <>
       <DrawerOverlay />
       <DrawerPrimitive.Content
         data-slot="drawer-content"
@@ -69,7 +69,7 @@ function DrawerContent({
         <div className="mx-auto mt-4 hidden h-2 w-[100px] shrink-0 rounded-full bg-muted group-data-[vaul-drawer-direction=bottom]/drawer-content:block" />
         {children}
       </DrawerPrimitive.Content>
-    </DrawerPortal>
+    </>
   )
 }
 
